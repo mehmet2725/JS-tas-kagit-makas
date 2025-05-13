196979b6937a8ca4010b7596e6e412cbaa7e54da
Erlik556 <234212007@ogrenci.amasya.edu.tr>
2025-05-14 00:26:54 +0300

    first commit

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+let kullaniciSecimi = prompt(`Taş, kağıt veya makas? Seçiminizi yazın:
+Lütfen hepsini küçük harflerle yazın:)`);
+
+let bilgisayarHamlesi = ["taş", "kağıt", "makas"].at(Math.floor(Math.random() * 3));
+
+if (kullaniciSecimi !== "taş" && kullaniciSecimi !== "kağıt" && kullaniciSecimi !== "makas") {
+    alert(`Geçersiz seçim! Lütfen 'taş', 'kağıt' veya 'makas' yazın.`);
+} else {
+    alert("Bilgisayarın seçimi: " + bilgisayarHamlesi);
+    
+    if (kullaniciSecimi === bilgisayarHamlesi) {
+        alert(`Berabere!`);
+    } 
+    else if (
+        (kullaniciSecimi === `taş` && bilgisayarHamlesi === `makas`) ||
+        (kullaniciSecimi === `kağıt` && bilgisayarHamlesi === `taş`) ||
+        (kullaniciSecimi === `makas` && bilgisayarHamlesi === `kağıt`)
+    ) {
+        alert(`Tebrikler, kazandınız!`);
+    } 
+    else if (
+        (kullaniciSecimi === `taş` && bilgisayarHamlesi === `kağıt`) ||
+        (kullaniciSecimi === `kağıt` && bilgisayarHamlesi === `makas`) ||
+        (kullaniciSecimi === `makas` && bilgisayarHamlesi === `taş`)
+    ) {
+        alert(`Bilgisayar kazandı!`);
+    }
+}
